3650e7e139acfa9932c1db75e5203bac6e17f701
Bruno Lanevik <bruno.lanevik@gmail.com>
2013-06-06 16:29:33 +0200

    Initial commit

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,27 @@
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        author: 'Sacha Greif',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        author: 'Tom Coleman',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        author: 'Tom Coleman',
+        url: 'http://themeteorbook.com'
+    },
+    {
+        title: 'Iveo',
+        author: ' ',
+        url: 'http://iveo.se'
+    }
+];
+Template.postsList.helpers({
+    posts: function() {
+        return Posts.find();
+    }
+});
